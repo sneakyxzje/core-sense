c74e381a99fc85a88965bb21d62714bc297fb021
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-16 17:43:34 +0700

    feat(submission): add submission service

---
+++ server/src/main/java/com/insight_pulse/tech/submission/service/SubmissionService.java
@@ -0,0 +1,32 @@
+package com.insight_pulse.tech.submission.service;
+
+import org.springframework.stereotype.Service;
+
+import com.insight_pulse.tech.campaign.domain.Campaign;
+import com.insight_pulse.tech.campaign.domain.CampaignRepository;
+import com.insight_pulse.tech.campaign.domain.CampaignStatus;
+import com.insight_pulse.tech.submission.domain.Submission;
+import com.insight_pulse.tech.submission.domain.SubmissionRepository;
+import com.insight_pulse.tech.submission.dto.SubmissionRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SubmissionService {
+
+    private final CampaignRepository campaignRepository;
+    private final SubmissionRepository submissionRepository;
+    public void submitForm(String campaignId, SubmissionRequest request) {
+        Campaign campaign = campaignRepository.findById(campaignId).orElseThrow(() -> new RuntimeException("Campaign not found"));
+        if (campaign.getStatus() != CampaignStatus.ACTIVE) {
+            throw new RuntimeException("Campaign này đã đóng hoặc chưa kích hoạt!");
+        }
+
+        Submission submission = new Submission();
+        submission.setCampaign(campaign);
+        submission.setAnswers(request.answers());
+
+        submissionRepository.save(submission);
+    }
+}
