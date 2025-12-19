5553b1d6055fd4255d74466749ad9ad5d3862536
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-19 17:48:26 +0700

    feat(gemini): add gemini request

---
+++ server/src/main/java/com/insight_pulse/tech/gemini/dto/GeminiRequest.java
@@ -0,0 +1,14 @@
+package com.insight_pulse.tech.gemini.dto;
+
+
+import java.util.List;
+import java.util.Map;
+
+import com.insight_pulse.tech.campaign.domain.FormQuestion;
+
+public record GeminiRequest(
+    List<FormQuestion> formSchema,
+    Map<String, Object> answer,
+    String userPrompt
+) {
+}
