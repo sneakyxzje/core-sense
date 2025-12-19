b372b38ba84b508e9500d4b5e5805b093ea9341f
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-19 17:41:36 +0700

    feat(submissions): add campaign schema loader for public submission

---
+++ client/src/routes/(public)/(p)/p/[campaignId]/+page.ts
@@ -0,0 +1,21 @@
+import { api } from "@src/lib/utils/api";
+import type { PageLoad } from "./$types";
+import type { CampaignPublic } from "@src/lib/types/campaign";
+
+export const load: PageLoad = async ({ params, fetch }) => {
+  const id = params.campaignId;
+  const res = await api.get<CampaignPublic>(
+    `/submission/campaign-schema/${id}`,
+    fetch
+  );
+
+  if (!res.formSchema) {
+    throw new Error("Không tìm thấy chiến dịch này hoặc link đã hết hạn!");
+  }
+
+  return {
+    campaignId: id,
+    campaignName: res.campaignName,
+    formSchema: res.formSchema,
+  };
+};
