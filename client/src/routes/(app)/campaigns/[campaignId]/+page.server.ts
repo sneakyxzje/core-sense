9e9223b772aa1594fea3b49a62e1f9530d9add32
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-21 17:24:40 +0700

    refactor(campaign): rename file

--- client/src/routes/(app)/campaigns/[campaignId]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from "./$types";
-import { api } from "@src/lib/utils/api";
-import type { CampaignDetail } from "@src/lib/types/campaign";
-
-export const load: PageServerLoad = async ({ fetch, params }) => {
-  const campaignId = params.campaignId;
-  const res = await api.get<CampaignDetail>(`/campaigns/${campaignId}`, fetch);
-  return { campaign: res };
-};
