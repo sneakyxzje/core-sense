15c3ea45a3a1a9266cfdfeab60dacd0a13f18db5
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-21 17:28:20 +0700

    feat(campaign): add pagination support in server load

--- client/src/routes/(app)/campaigns/+page.server.ts
+++ client/src/routes/(app)/campaigns/+page.server.ts
@@ -2,10 +2,19 @@ import { api } from "@src/lib/utils/api.js";
 import type { PageServerLoad } from "./$types";
 import type { Campaign } from "@src/lib/types/campaign";
 
-export const load: PageServerLoad = async ({ fetch }) => {
-  const res = await api.get<Campaign[]>("/campaigns", fetch);
+export const load: PageServerLoad = async ({ url, fetch }) => {
+  const page = url.searchParams.get("page") || "0";
+  const size = url.searchParams.get("size") || "5";
+  const res = await api.get<PageResponse<Campaign>>(
+    `/campaigns?page=${page}&size=${size}`,
+    fetch
+  );
 
   return {
-    campaigns: res,
+    campaigns: res.content,
+    currentPage: parseInt(page),
+    size: parseInt(size),
+    totalElements: res.totalElements,
+    totalPages: res.totalPages,
   };
 };
