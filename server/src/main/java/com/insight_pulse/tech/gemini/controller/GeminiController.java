b3807b86e36f52b615e911bde6bc79a3b0dc6006
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-22 18:27:44 +0700

    feat(gemini): add compare endpoint

--- server/src/main/java/com/insight_pulse/tech/gemini/controller/GeminiController.java
+++ server/src/main/java/com/insight_pulse/tech/gemini/controller/GeminiController.java
@@ -1,5 +1,6 @@
 package com.insight_pulse.tech.gemini.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.insight_pulse.tech.gemini.dto.GeminiResponse;
+import com.insight_pulse.tech.submission.dto.SubmissionResponse;
 import com.insight_pulse.tech.submission.service.SubmissionService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,10 +22,17 @@ import lombok.RequiredArgsConstructor;
 public class GeminiController {
 
     private final SubmissionService submissionService;
+
     @PostMapping("/{id}/analyze") 
     public ResponseEntity<GeminiResponse> analyze(@PathVariable String id, @RequestBody Map<String, String> body) {
-    String prompt = body.getOrDefault("userPrompts", "Mặc định...");
-    GeminiResponse result = submissionService.analyzeAndSave(id, prompt);
-    return ResponseEntity.ok(result);
-}
+        String prompt = body.getOrDefault("userPrompts", "Mặc định...");
+        GeminiResponse result = submissionService.analyzeAndSave(id, prompt);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/compare")
+    public ResponseEntity<GeminiResponse> compare(@RequestBody List<SubmissionResponse> request) {
+        GeminiResponse result = submissionService.compare(request); 
+        return ResponseEntity.ok(result);
+    }
 }
