0f3b9286567c52e2568185aa8cedf8142857f648
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-26 16:10:28 +0700

    style(ui): change css for detail submission

--- client/src/routes/(app)/campaigns/[campaignId]/components/SubmissionDetail.svelte
+++ client/src/routes/(app)/campaigns/[campaignId]/components/SubmissionDetail.svelte
@@ -14,10 +14,12 @@
 
 <Sheet.Root bind:open={isSheetOpen}>
   <Sheet.Content
-    class="sm:max-w-xl w-[95vw] p-0 flex flex-col h-full border-l shadow-2xl overflow-hidden sm:duration-500"
+    class="sm:max-w-xl w-[95vw] p-0 flex flex-col h-full border-l border-base-border-1 bg-base-3 shadow-2xl overflow-hidden sm:duration-500"
   >
     {#if selectedSubmission}
-      <div class="relative overflow-hidden border-b bg-background px-6 py-8">
+      <div
+        class="relative overflow-hidden border-b border-base-border-1 bg-background px-6 py-8"
+      >
         <div class="relative space-y-4">
           <div class="flex items-start justify-between gap-4">
             <div class="space-y-1.5">
@@ -31,9 +33,9 @@
               </Sheet.Title>
               <div class="flex items-center gap-3 text-slate-500">
                 <div
-                  class="flex items-center gap-1.5 bg-white px-2 py-1 rounded-md border border-slate-200 shadow-sm text-xs font-medium"
+                  class="flex items-center gap-1.5 bg-base-2 px-2 py-1 rounded-md border border-base-border-1 shadow-sm text-xs font-medium"
                 >
-                  <Calendar class="w-3.5 h-3.5 text-indigo-500" />
+                  <Calendar class="w-3.5 h-3.5 " />
                   {new Date(selectedSubmission.submittedAt).toLocaleDateString(
                     "vi-VN"
                   )}
@@ -52,7 +54,7 @@
             </div>
 
             <div
-              class={`flex flex-col items-center justify-center min-w-[70px] p-2  border-2 shadow-sm ${getScoreColor(selectedSubmission.score)} bg-white`}
+              class={`flex flex-col items-center justify-center min-w-[70px] p-2  border-2 shadow-sm ${getScoreColor(selectedSubmission.score)} rounded-md `}
             >
               <span
                 class="text-[10px] uppercase font-bold opacity-70 leading-none mb-1"
@@ -72,23 +74,20 @@
         <div class="p-6 space-y-10 pb-32">
           <div class="group relative">
             <div
-              class="absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-2xl blur opacity-10 group-hover:opacity-20 transition duration-500"
+              class="absolute -inset-0.5 rounded-2xl blur opacity-10 group-hover:opacity-20 transition duration-500"
             ></div>
             <div
-              class="relative space-y-3 bg-white border border-indigo-100 rounded-xl p-5 shadow-sm"
+              class="relative space-y-3 bg-base-3 border border-base-border-1 rounded-xl p-5 shadow-sm"
             >
               <div class="flex items-center justify-between">
                 <h4
-                  class="text-[11px] font-black text-indigo-600 uppercase tracking-[0.2em] flex items-center gap-2"
+                  class="text-[12px] text-base-fg-1 font-bold uppercase flex items-center gap-2"
                 >
-                  <div class="p-1 bg-indigo-100 rounded-lg">
-                    <Sparkles class="w-3.5 h-3.5" />
-                  </div>
                   Tóm tắt phân tích của AI
                 </h4>
               </div>
 
-              <div class="text-sm leading-7 text-slate-700">
+              <div class="text-sm leading-7 text-base-fg-2">
                 {#if selectedSubmission.aiAssessment}
                   <p>
                     {selectedSubmission.aiAssessment?.summary}
@@ -114,13 +113,12 @@
               >
                 Chi tiết câu trả lời
               </h4>
-              <div class="h-[1px] w-full bg-slate-100"></div>
             </div>
 
             <div class="space-y-6">
               {#each getMappedAnswers(selectedSubmission.answer, campaign.formSchema) as item, i}
                 <div
-                  class="relative pl-6 border-l-2 border-slate-100 hover:border-indigo-200 transition-colors"
+                  class="relative pl-6 border-l-2 border-base-border-2 hover:border-base-border-hover transition-colors"
                 >
                   <span
                     class="absolute -left-[9px] top-0 w-4 h-4 rounded-full bg-white border-2 border-slate-200 text-[9px] font-bold flex items-center justify-center text-slate-400"
@@ -135,7 +133,7 @@
                       {item.label}
                     </span>
                     <div
-                      class="text-sm text-primary-900 leading-relaxed bg-background p-4 rounded-xl border border-slate-100/50 whitespace-pre-wrap"
+                      class="text-sm text-primary-900 leading-relaxed bg-background p-4 rounded-xl border border-base-border-2 whitespace-pre-wrap"
                     >
                       {item.value}
                     </div>
@@ -148,7 +146,7 @@
       </div>
 
       <div
-        class="absolute bottom-0 left-0 right-0 p-5 bg-background backdrop-blur-md border-t flex justify-end gap-3 z-30 shadow-[0_-10px_20px_rgba(0,0,0,0,0.02)]"
+        class="absolute bottom-0 left-0 right-0 p-5 bg-background backdrop-blur-md border-t border-base-border-2 flex justify-end gap-3 z-30 shadow-[0_-10px_20px_rgba(0,0,0,0,0.02)]"
       >
         <Button
           variant="ghost"
@@ -158,7 +156,7 @@
           Đóng
         </Button>
         <Button
-          class="bg-slate-900  hover:bg-slate-800 text-white px-6 rounded-xl transition-all active:scale-95"
+          class="bg-primary-1  hover:bg-primary-hover text-primary-fg-1 px-6 rounded-xl transition-all active:scale-95"
           onclick={() =>
             goto(`${campaign.id}/submissions/${selectedSubmission.id}`)}
         >
