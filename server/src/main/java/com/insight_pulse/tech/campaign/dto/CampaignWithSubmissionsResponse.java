16e97e0e030442c38361452d9dbf34139fed20af
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-28 17:01:51 +0700

    feat(campaign): return paginated submissions in campaign response

--- server/src/main/java/com/insight_pulse/tech/campaign/dto/CampaignWithSubmissionsResponse.java
+++ server/src/main/java/com/insight_pulse/tech/campaign/dto/CampaignWithSubmissionsResponse.java
@@ -1,10 +1,10 @@
 package com.insight_pulse.tech.campaign.dto;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
 
 import com.insight_pulse.tech.submission.dto.SubmissionResponse;
 
 public record CampaignWithSubmissionsResponse(
     CampaignDetailResponse campaign,
-    List<SubmissionResponse> submissions
+    Page<SubmissionResponse> submissions
 ) {}
