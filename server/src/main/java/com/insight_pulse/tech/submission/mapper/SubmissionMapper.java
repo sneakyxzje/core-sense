6428ed39980961fb8633f36d724778b554430f80
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-28 17:03:15 +0700

    feat(submission): add submission mapper

---
+++ server/src/main/java/com/insight_pulse/tech/submission/mapper/SubmissionMapper.java
@@ -0,0 +1,34 @@
+package com.insight_pulse.tech.submission.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.insight_pulse.tech.submission.domain.Submission;
+import com.insight_pulse.tech.submission.dto.SubmissionDetailResponse;
+import com.insight_pulse.tech.submission.dto.SubmissionResponse;
+
+@Component
+public class SubmissionMapper {
+    
+
+    public SubmissionResponse toResponse(Submission s) {
+        return new SubmissionResponse(
+            s.getId(),
+            s.getAiAssessment(),
+            s.getAnswers(),
+            s.getScore(),
+            s.getSubmittedAt()
+        );
+    }
+
+    public SubmissionDetailResponse toDetailResponse(Submission s, String userPrompts) {
+        return new SubmissionDetailResponse(
+            userPrompts,
+            s.getId(),
+            s.getAiAssessment(),
+            s.getAnswers(),
+            s.getScore(),
+            s.getSubmittedAt(),
+            s.getSchemaSnapshot()
+        );
+    }
+}
