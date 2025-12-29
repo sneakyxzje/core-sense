8c9dd2e6b444640b551511524e5031a6e2e52a01
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-29 18:16:12 +0700

    feat(campaign): load stages and submissions in parallel for campaign detail

--- client/src/routes/(app)/campaigns/[campaignId]/+layout.server.ts
+++ client/src/routes/(app)/campaigns/[campaignId]/+layout.server.ts
@@ -1,21 +1,29 @@
 import { api } from "@src/lib/utils/api";
 import type { LayoutServerLoad } from "./$types";
-import type { CampaignWithSubmission } from "@src/lib/types/campaign";
+import {
+  type CampaignStage,
+  type CampaignWithSubmission,
+} from "@src/lib/types/campaign";
 
 export const load: LayoutServerLoad = async ({ fetch, params, url }) => {
   const campaignId = params.campaignId;
   const page = url.searchParams.get("page") || "0";
   const size = url.searchParams.get("size") || "10";
   const search = url.searchParams.get("search") || "";
-  const res = await api.get<CampaignWithSubmission>(
-    `/campaigns/${campaignId}/submissions?page=${page}&size=${size}&search=${search}&sort=submittedAt,desc`,
-    fetch
-  );
-  const campaignName = res.campaign.name;
+  const [campaignStages, campaignWithSubmission] = await Promise.all([
+    api.get<CampaignStage[]>(`/campaigns/${campaignId}/stages`, fetch),
+    api.get<CampaignWithSubmission>(
+      `/campaigns/${campaignId}/submissions?page=${page}&size=${size}&search=${search}&sort=submittedAt,desc`,
+      fetch
+    ),
+  ]);
+
+  const campaignName = campaignWithSubmission.campaign.name;
   return {
-    campaign: res.campaign,
-    submissions: res.submissions,
+    campaign: campaignWithSubmission.campaign,
+    submissions: campaignWithSubmission.submissions,
     campaignName,
     campaignId,
+    stages: campaignStages,
   };
 };
