2475bfbd87f27d6217d59d45159a28688da90508
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-29 18:27:09 +0700

    feat(submission): update SubmissionRequest to use AnswerDetail instead of raw map

--- server/src/main/java/com/insight_pulse/tech/submission/dto/SubmissionRequest.java
+++ server/src/main/java/com/insight_pulse/tech/submission/dto/SubmissionRequest.java
@@ -1,9 +1,9 @@
 package com.insight_pulse.tech.submission.dto;
 
-import java.util.Map;
+
 
 public record SubmissionRequest(
-    Map<String, Object> answers
+    AnswerDetail answers
 ) {
     
 }
