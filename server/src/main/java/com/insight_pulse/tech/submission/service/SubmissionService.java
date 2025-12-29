a60859edfaca3dd6661943cf83f97aed63ed586a
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-29 18:28:27 +0700

    feat(submission): enhance submission flow with user info, stages, and unified query handling

--- server/src/main/java/com/insight_pulse/tech/submission/service/SubmissionService.java
+++ server/src/main/java/com/insight_pulse/tech/submission/service/SubmissionService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,17 +18,24 @@ import com.insight_pulse.tech.campaign.domain.Campaign;
 import com.insight_pulse.tech.campaign.domain.CampaignRepository;
 import com.insight_pulse.tech.campaign.domain.CampaignStatus;
 import com.insight_pulse.tech.campaign.domain.FormQuestion;
+import com.insight_pulse.tech.campaign.dto.CampaignDetailResponse;
+import com.insight_pulse.tech.campaign.dto.CampaignWithSubmissionsResponse;
 import com.insight_pulse.tech.campaign.dto.PublicCampaignResponse;
+import com.insight_pulse.tech.campaign.mapper.CampaignMapper;
 import com.insight_pulse.tech.gemini.dto.GeminiRequest;
 import com.insight_pulse.tech.gemini.dto.GeminiResponse;
 import com.insight_pulse.tech.gemini.service.GeminiService;
 import com.insight_pulse.tech.security.context.CurrentUserProvider;
 import com.insight_pulse.tech.submission.domain.Submission;
 import com.insight_pulse.tech.submission.domain.SubmissionRepository;
+import com.insight_pulse.tech.submission.dto.SubmissionChart;
+import com.insight_pulse.tech.submission.dto.SubmissionDetailResponse;
 import com.insight_pulse.tech.submission.dto.SubmissionEvent;
 import com.insight_pulse.tech.submission.dto.SubmissionRequest;
 import com.insight_pulse.tech.submission.dto.SubmissionResponse;
 import com.insight_pulse.tech.submission.dto.SubmissionSummary;
+import com.insight_pulse.tech.submission.mapper.SubmissionMapper;
+import com.insight_pulse.tech.utils.PageableUtils;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -42,6 +50,9 @@ public class SubmissionService {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final SimpMessagingTemplate messagingTemplate;
     private final CurrentUserProvider currentUserProvider;
+    private final CampaignMapper campaignMapper;
+    private final SubmissionMapper submissionMapper;
+
 
     @Transactional
     public void submitForm(String campaignId, SubmissionRequest request) {
@@ -51,8 +62,10 @@ public class SubmissionService {
         }
         List<FormQuestion> schemaSnapshot = campaign.getFormSchema();
         Submission submission = new Submission();
+        submission.setFullname(request.answers().getSysName());
+        submission.setEmail(request.answers().getSysEmail());
         submission.setCampaign(campaign);
-        submission.setAnswers(request.answers());
+        submission.setAnswers(request.answers().getAllAnswers());
         submission.setSchemaSnapshot(schemaSnapshot);
         submissionRepository.save(submission);
         campaignRepository.incrementTotalSubmissions(campaignId);
@@ -78,6 +91,8 @@ public class SubmissionService {
         );
     }
 
+    
+
     public Page<SubmissionSummary> getSubmissionSummary() {
         int userId = currentUserProvider.getCurrentUserId();
         Pageable top5 = PageRequest.of(0, 5, Sort.by("submittedAt").descending());
@@ -86,6 +101,40 @@ public class SubmissionService {
             s.getCampaign().getName(),
             s.getSubmittedAt()
         ));
+    }
+
+    public CampaignWithSubmissionsResponse findSubmissionByCampaign(String campaignId, String search, Pageable pageable) {
+        int userId = currentUserProvider.getCurrentUserId();
+        Campaign campaign = campaignRepository.findByIdAndUserId(campaignId, userId).orElseThrow(() -> new RuntimeException("Campaign not found or permission denied"));
+        Page<Submission> submissions;
+
+        if(StringUtils.hasText(search)) {
+            Pageable nativePageable = PageableUtils.convertToNativePageable(pageable);
+            submissions = submissionRepository.searchSubmission(campaignId, search.trim(), nativePageable);
+        }
+        else {
+            submissions = submissionRepository.findAllByCampaignId(campaignId, pageable);
+        }
+        
+        CampaignDetailResponse campaignResponse = campaignMapper.toDetailResponse(campaign);
+        Page<SubmissionResponse> submissionResponse = submissions.map(submissionMapper::toResponse);
+        return new CampaignWithSubmissionsResponse(
+            campaignResponse,
+            submissionResponse
+        );
+    }
+
+    public List<SubmissionChart> getSubmissionChart() {
+        int userId = currentUserProvider.getCurrentUserId();
+        return campaignRepository.getSubmissionChartData(userId);
+    }
+
+    public SubmissionDetailResponse getSubmissionDetailByCampaign(String campaignId, String submissionId) {
+        int userId = currentUserProvider.getCurrentUserId();
+        Campaign campaign = campaignRepository.findByIdAndUserId(campaignId, userId).orElseThrow(() -> new RuntimeException("Campaign not found or permission denied"));
+        String userPrompts = campaign.getAiSystemPrompt();
+        Submission submission = submissionRepository.findSubmissionDetail(userId, campaignId, submissionId);
+        return submissionMapper.toDetailResponse(submission, userPrompts);
     }
 
 
