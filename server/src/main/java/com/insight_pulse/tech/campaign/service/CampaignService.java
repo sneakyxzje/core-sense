4af181d88679828e85be5785a0f0522c05a05bad
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-29 18:25:21 +0700

    refactor(campaign): extract submission logic, add campaign stage management, and simplify service responsibilities

--- server/src/main/java/com/insight_pulse/tech/campaign/service/CampaignService.java
+++ server/src/main/java/com/insight_pulse/tech/campaign/service/CampaignService.java
@@ -1,33 +1,31 @@
 package com.insight_pulse.tech.campaign.service;
 
 import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
 import com.insight_pulse.tech.campaign.domain.Campaign;
 import com.insight_pulse.tech.campaign.domain.CampaignRepository;
+import com.insight_pulse.tech.campaign.domain.CampaignStage;
+import com.insight_pulse.tech.campaign.domain.CampaignStageRepository;
 import com.insight_pulse.tech.campaign.domain.CampaignStatus;
 import com.insight_pulse.tech.campaign.dto.CampaignDetailResponse;
 import com.insight_pulse.tech.campaign.dto.CampaignRequest;
 import com.insight_pulse.tech.campaign.dto.CampaignResponse;
+import com.insight_pulse.tech.campaign.dto.CampaignStageRequest;
+import com.insight_pulse.tech.campaign.dto.CampaignStageResponse;
 import com.insight_pulse.tech.campaign.dto.CampaignStats;
-import com.insight_pulse.tech.campaign.dto.CampaignWithSubmissionsResponse;
 import com.insight_pulse.tech.campaign.dto.UpdateCampaignRequest;
+import com.insight_pulse.tech.campaign.dto.UpdateStageNameRequest;
 import com.insight_pulse.tech.campaign.mapper.CampaignMapper;
 import com.insight_pulse.tech.exception.ResourceNotFoundException;
 import com.insight_pulse.tech.security.context.CurrentUserProvider;
-import com.insight_pulse.tech.submission.domain.Submission;
-import com.insight_pulse.tech.submission.domain.SubmissionRepository;
-import com.insight_pulse.tech.submission.dto.SubmissionChart;
-import com.insight_pulse.tech.submission.dto.SubmissionDetailResponse;
-import com.insight_pulse.tech.submission.dto.SubmissionResponse;
-import com.insight_pulse.tech.submission.mapper.SubmissionMapper;
+
 import com.insight_pulse.tech.user.domain.User;
 import com.insight_pulse.tech.user.domain.UserRepository;
-import com.insight_pulse.tech.utils.PageableUtils;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -35,13 +33,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CampaignService {
 
-    
     private final CampaignRepository campaignRepository;
     private final UserRepository userRepository;
-    private final SubmissionRepository submissionRepository;
     private final CurrentUserProvider currentUserProvider;
     private final CampaignMapper campaignMapper;
-    private final SubmissionMapper submissionMapper;
+    private final CampaignStageRepository campaignStageRepository;
+
 
     private Campaign getOwnedCampaign(String campaignId) {
         int userId = currentUserProvider.getCurrentUserId();
@@ -62,6 +59,37 @@ public class CampaignService {
         return campaignMapper.toResponse(campaignRepository.save(campaign));
     }
 
+    public CampaignStageResponse createStage(String campaignId, CampaignStageRequest request) {
+        int userId = currentUserProvider.getCurrentUserId();
+        Campaign campaign = campaignRepository.findByIdAndUserId(campaignId, userId).orElseThrow(() -> new RuntimeException("Campaign not found or permission denied"));
+
+        Integer maxPosition = campaignStageRepository.findMaxPositionByCampaignId(campaignId);
+        int nextPosition = (maxPosition == null) ? 0 : maxPosition + 1;
+        CampaignStage campaignStage = new CampaignStage();
+        campaignStage.setCampaign(campaign);
+        campaignStage.setPosition(nextPosition);
+        campaignStage.setStageName(request.stageName());
+        campaignStageRepository.save(campaignStage);
+        return new CampaignStageResponse(
+            campaignStage.getId(),
+            request.stageName(),
+            campaign.getId(),
+            nextPosition
+        );
+    }
+
+    public List<CampaignStageResponse> getAllStage(String campaignId) {
+        int userId = currentUserProvider.getCurrentUserId();
+        List<CampaignStage> campaign = campaignStageRepository.findAllByCampaignIdAndCampaignUserId(campaignId, userId);
+
+        return campaign.stream().map((s) -> new CampaignStageResponse(
+            s.getId(),
+            s.getStageName(),
+            s.getCampaign().getId(),
+            s.getPosition()
+        )).toList();
+    }
+
     public CampaignStats getCampaignInfo() {
         int userId = currentUserProvider.getCurrentUserId();
         Double ratio = campaignRepository.calculateHightQualityRatio(userId);
@@ -71,12 +99,6 @@ public class CampaignService {
             ratio != null ? ratio : 0.0
         );
     }
-
-    public List<SubmissionChart> getSubmissionChart() {
-        int userId = currentUserProvider.getCurrentUserId();
-        return campaignRepository.getSubmissionChartData(userId);
-    }
-
     public Page<CampaignResponse> getCampaigns(Pageable pageable) {
         int userId = currentUserProvider.getCurrentUserId();
         Page<Campaign> campaigns = campaignRepository.findAllByUserId(userId, pageable);
@@ -88,13 +110,7 @@ public class CampaignService {
         return campaignMapper.toDetailResponse(campaign);
     }
 
-    public SubmissionDetailResponse getSubmissionDetailByCampaign(String campaignId, String submissionId) {
-        int userId = currentUserProvider.getCurrentUserId();
-        Campaign campaign = campaignRepository.findByIdAndUserId(campaignId, userId).orElseThrow(() -> new RuntimeException("Campaign not found or permission denied"));
-        String userPrompts = campaign.getAiSystemPrompt();
-        Submission submission = submissionRepository.findSubmissionDetail(userId, campaignId, submissionId);
-        return submissionMapper.toDetailResponse(submission, userPrompts);
-    }
+
 
     public void toggleCampaignStatus(String campaignId, Boolean enabled) {        
         Campaign campaign = getOwnedCampaign(campaignId);
@@ -114,25 +130,20 @@ public class CampaignService {
         return campaignMapper.toDetailResponse(campaign);
     }
 
-
-    public CampaignWithSubmissionsResponse findSubmissionByCampaign(String campaignId, String search, Pageable pageable) {
+    @Transactional
+    public CampaignStageResponse updateStageName(String stageId, UpdateStageNameRequest request) {
         int userId = currentUserProvider.getCurrentUserId();
-        Campaign campaign = campaignRepository.findByIdAndUserId(campaignId, userId).orElseThrow(() -> new RuntimeException("Campaign not found or permission denied"));
-        Page<Submission> submissions;
-
-        if(StringUtils.hasText(search)) {
-            Pageable nativePageable = PageableUtils.convertToNativePageable(pageable);
-            submissions = submissionRepository.searchSubmission(campaignId, search.trim(), nativePageable);
+        CampaignStage campaignStage = campaignStageRepository.findById(stageId)
+        .orElseThrow(() -> new EntityNotFoundException("Không tìm thấy cột này"));
+        if (campaignStage.getCampaign().getUser().getId() != userId) {
+            throw new RuntimeException("Bạn không có quyền sửa cột này");
         }
-        else {
-            submissions = submissionRepository.findAllByCampaignId(campaignId, pageable);
-        }
-        
-        CampaignDetailResponse campaignResponse = campaignMapper.toDetailResponse(campaign);
-        Page<SubmissionResponse> submissionResponse = submissions.map(submissionMapper::toResponse);
-        return new CampaignWithSubmissionsResponse(
-            campaignResponse,
-            submissionResponse
+        campaignStage.setStageName(request.stageName());
+        return new CampaignStageResponse(
+            campaignStage.getId(),
+            campaignStage.getStageName(),
+            campaignStage.getCampaign().getId(),
+            campaignStage.getPosition()
         );
     }
 }
