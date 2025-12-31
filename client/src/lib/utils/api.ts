363abf1650c890272fd053d52c605a16f8004bda
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-31 15:24:36 +0700

    feat(api): add centralized request handler with token refresh and auto-redirect on auth failure

--- client/src/lib/utils/api.ts
+++ client/src/lib/utils/api.ts
@@ -1,3 +1,5 @@
+import { browser } from "$app/environment";
+
 const BASE_URL = "http://localhost:8080/api";
 export type Fetch = typeof fetch;
 async function handleResponse<T>(response: Response): Promise<T> {
@@ -24,17 +26,53 @@ async function handleResponse<T>(response: Response): Promise<T> {
   return data as T;
 }
 
+async function refreshAccessToken(customFetch: Fetch): Promise<boolean> {
+  try {
+    const res = await customFetch("http://localhost:8080/api/auth/refresh", {
+      method: "POST",
+      credentials: "include",
+    });
+    return res.ok;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function request<T>(
+  endpoint: String,
+  options: RequestInit,
+  customFetch: Fetch = fetch,
+  isRetry = false
+): Promise<T> {
+  const url = `${BASE_URL}${endpoint}`;
+  const finalOptions = {
+    ...options,
+    credentials: "include" as RequestCredentials,
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+  };
+
+  const response = await customFetch(url, finalOptions);
+  if ((response.status === 401 || response.status === 403) && !isRetry) {
+    const refreshSuccess = await refreshAccessToken(customFetch);
+
+    if (refreshSuccess) {
+      return request<T>(endpoint, options, customFetch, true);
+    } else {
+      if (browser) {
+        window.location.href = "/login";
+      }
+    }
+  }
+  return handleResponse<T>(response);
+}
+
 export const api = {
   get: async <T>(endpoint: string, customFetch: Fetch = fetch): Promise<T> => {
     const url = `${BASE_URL}${endpoint}`;
-    const response = await customFetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
-    return handleResponse<T>(response);
+    return request<T>(endpoint, { method: "GET" }, customFetch);
   },
 
   post: async <T, D>(
@@ -42,16 +80,11 @@ export const api = {
     data: D,
     customFetch: Fetch = fetch
   ): Promise<T> => {
-    const url = `${BASE_URL}${endpoint}`;
-    const response = await customFetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      credentials: "include",
-    });
-    return handleResponse<T>(response);
+    return request<T>(
+      endpoint,
+      { method: "POST", body: JSON.stringify(data) },
+      customFetch
+    );
   },
 
   put: async <T, D>(
@@ -59,16 +92,11 @@ export const api = {
     data: D,
     customFetch: Fetch = fetch
   ): Promise<T> => {
-    const url = `${BASE_URL}${endpoint}`;
-    const response = await customFetch(url, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      credentials: "include",
-    });
-    return handleResponse<T>(response);
+    return request<T>(
+      endpoint,
+      { method: "PUT", body: JSON.stringify(data) },
+      customFetch
+    );
   },
 
   patch: async <T, D>(
@@ -76,30 +104,22 @@ export const api = {
     data: D,
     customFetch: Fetch = fetch
   ): Promise<T> => {
-    const url = `${BASE_URL}${endpoint}`;
-    const response = await customFetch(url, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      credentials: "include",
-    });
-    return handleResponse<T>(response);
+    return request<T>(
+      endpoint,
+      { method: "PATCH", body: JSON.stringify(data) },
+      customFetch
+    );
   },
 
-  delete: async <T>(
+  delete: async <T, D>(
     endpoint: string,
+    data: D,
     customFetch: Fetch = fetch
   ): Promise<T> => {
-    const url = `${BASE_URL}${endpoint}`;
-    const response = await customFetch(url, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
-    return handleResponse<T>(response);
+    return request<T>(
+      endpoint,
+      { method: "DELETE", body: JSON.stringify(data) },
+      customFetch
+    );
   },
 };
