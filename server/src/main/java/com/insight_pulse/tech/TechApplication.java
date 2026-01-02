a8a529c12d8dc76ea2c08f81fca67c7fa47fc0ce
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-02 20:29:37 +0700

    feat(app): enable scheduling support

--- server/src/main/java/com/insight_pulse/tech/TechApplication.java
+++ server/src/main/java/com/insight_pulse/tech/TechApplication.java
@@ -3,11 +3,13 @@ package com.insight_pulse.tech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.insight_pulse.tech.security.config.JwtConfigProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtConfigProperties.class)
+@EnableScheduling
 public class TechApplication {
 
 	public static void main(String[] args) {
