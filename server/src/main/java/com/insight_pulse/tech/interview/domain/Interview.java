05403a210910dd7c57f454bad675ffb2df4bee20
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-02 20:31:24 +0700

    feat(interview): add interview entity and domain logic

---
+++ server/src/main/java/com/insight_pulse/tech/interview/domain/Interview.java
@@ -0,0 +1,63 @@
+package com.insight_pulse.tech.interview.domain;
+
+import java.time.LocalDateTime;
+
+import com.insight_pulse.tech.submission.domain.Submission;
+import com.insight_pulse.tech.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="interviews")
+public class Interview {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name="itv_submission_id", nullable = false)
+    private Submission submission;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @Column(name ="schedule_at", nullable = false)
+    private LocalDateTime scheduleAt;
+
+    @Column(name ="reminder_sent")
+    private boolean reminderSent = false;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "notes", columnDefinition = "TEXT")
+    private String notes;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public static Interview create(Submission submission, LocalDateTime scheduleAt, String location, String notes, User user) {
+        if(submission == null) throw new IllegalArgumentException("Submission not found");
+        if(scheduleAt == null) throw new IllegalArgumentException("Schedule is null!");
+        if(scheduleAt.isBefore(LocalDateTime.now())) throw new IllegalArgumentException("Schedule cannot be in the past");
+
+        Interview interview = new Interview();
+        interview.setSubmission(submission);
+        interview.setScheduleAt(scheduleAt);
+        interview.setLocation(location);
+        interview.setNotes(notes);
+        interview.setUser(user);
+        return interview;
+    }
+}
