a2ed87f22fe9a39e29f6102da38706650857167b
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-02 20:27:54 +0700

    feat(socket): init websocket client and handle notifications in layout

--- client/src/routes/(app)/+layout.svelte
+++ client/src/routes/(app)/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
   import Sidebar from "@src/lib/components/Sidebar.svelte";
   import * as Sidebars from "$lib/components/ui/sidebar/index.js";
+  import { createSocketClient } from "@src/lib/services/Socket";
+  import { notificationStore } from "@src/lib/stores/notification.svelte";
   let { children } = $props();
+
+  $effect(() => {
+    const client = createSocketClient(
+      (sub) => console.log("Sub:", sub),
+      (noti) => {
+        notificationStore.add(noti);
+      }
+    );
+
+    client.activate();
+    return () => client.deactivate();
+  });
 </script>
 
 <Sidebars.Provider>
