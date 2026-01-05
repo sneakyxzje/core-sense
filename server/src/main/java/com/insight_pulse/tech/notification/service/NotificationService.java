8619eb29a256fb0172a266bcaf66ca7d756c6442
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-05 13:45:05 +0700

    feat(notification): add service method to fetch user notifications with unread count

--- server/src/main/java/com/insight_pulse/tech/notification/service/NotificationService.java
+++ server/src/main/java/com/insight_pulse/tech/notification/service/NotificationService.java
@@ -1,11 +1,16 @@
 package com.insight_pulse.tech.notification.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.insight_pulse.tech.notification.domain.Notification;
 import com.insight_pulse.tech.notification.domain.NotificationRepository;
+import com.insight_pulse.tech.notification.dto.NotificationSummary;
 import com.insight_pulse.tech.notification.dto.NotificationRequest;
+import com.insight_pulse.tech.notification.dto.NotificationResponse;
+import com.insight_pulse.tech.security.context.CurrentUserProvider;
 import com.insight_pulse.tech.user.domain.User;
 import com.insight_pulse.tech.user.domain.UserRepository;
 
@@ -18,6 +23,7 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final SimpMessagingTemplate messagingTemplate;
     private final UserRepository userRepository;
+    private final CurrentUserProvider currentUserProvider;
     public void createNotification(String title, String message, int userId, String type, String link) {
         if (title == null) throw new IllegalArgumentException("Title cannot be null!");
         if (message == null) throw new IllegalArgumentException("Message cannot be null!");
@@ -40,4 +46,23 @@ public class NotificationService {
                     response
         );
     } 
+
+    public NotificationSummary getNotifications(Pageable pageable) {
+        int userId = currentUserProvider.getCurrentUserId(); 
+        Page<Notification> notifications = notificationRepository.findAllByUserId(pageable, userId);        
+        long unreadCount = notificationRepository.countByUserIdAndIsReadFalse(userId);
+        Page<NotificationResponse> notificationResponse = notifications.map(n -> new NotificationResponse(
+            n.getId(),
+            n.getTitle(),
+            n.getMessage(),
+            n.getLink(),
+            n.getType(),
+            n.isRead(),
+            n.getCreatedAt()
+        ));
+        return new NotificationSummary(
+            notificationResponse,
+            unreadCount
+        ); 
+    }
 }
