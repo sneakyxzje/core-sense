75336a2ba6cf2679e4e25ab525c9f1298e567265
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-05 17:28:17 +0700

    refactor: relocate submission and assessment types from campaign to submission types file

--- client/src/lib/types/submission.ts
+++ client/src/lib/types/submission.ts
@@ -1,3 +1,5 @@
+import type { FormQuestion } from "@src/lib/types/FormQuestion";
+
 export type TotalSubmissions = {
   campaignId: string;
   campaignName: string;
@@ -22,4 +24,35 @@ export type SubmissionEvent = {
 export type SubmissionSummary = {
   campaignName: string;
   submittedAt: string;
+};
+
+export interface Submission {
+  userPrompts: string;
+  id: string;
+  fullName: string;
+  aiAssessment: Record<string, any>;
+  answer: Record<string, any>;
+  score: number;
+  submittedAt: string;
+  summary: string;
+}
+
+export interface SubmissionWithStage extends Submission {
+  stageId: string;
+  starred: boolean;
+}
+
+export interface SubmissionDetail extends Submission {
+  snapshotSchema: FormQuestion[];
+  cvUrl: string;
+}
+
+export interface AiAssessment {
+  score: number;
+  aiAssesment: string;
+  highlights: {
+    text: string;
+    type: "positive" | "negative" | "neutral";
+    comment: string;
+  }[];
 }
