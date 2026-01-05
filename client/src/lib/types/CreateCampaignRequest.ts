470feb31b3f1924e20f41709058ff8b091de42d3
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-05 17:27:38 +0700

    chore(campaign): move file to dedicated file

--- client/src/lib/types/CreateCampaignRequest.ts
+++
@@ -1,8 +0,0 @@
-import type { FormQuestion } from "@src/lib/types/FormQuestion";
-
-export interface CreateCampaignRequest {
-  name: string;
-  description: string;
-  aiSystemPrompt: string;
-  formSchema: FormQuestion[];
-}
