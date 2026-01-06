b953489603341a132acc209d7e0013049a47b0d3
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-06 22:45:30 +0700

    feat(notification): add find notification by id and user id

--- server/src/main/java/com/insight_pulse/tech/notification/domain/NotificationRepository.java
+++ server/src/main/java/com/insight_pulse/tech/notification/domain/NotificationRepository.java
@@ -1,5 +1,7 @@
 package com.insight_pulse.tech.notification.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +11,6 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     Page<Notification> findAllByUserId(Pageable pageable, int userId);
 
     long countByUserIdAndIsReadFalse(int userId);
+
+    Optional<Notification> findByIdAndUserId(long id, int userId);
 }
