5cfc33573537cefeb037ed135beb76cfffc42aef
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-07 23:22:24 +0700

    feat(campaign): add campaign setting service

---
+++ server/src/main/java/com/insight_pulse/tech/campaign/service/CampaignSettingsService.java
@@ -0,0 +1,19 @@
+package com.insight_pulse.tech.campaign.service;
+
+import org.springframework.stereotype.Service;
+
+import com.insight_pulse.tech.automation.service.AutomationService;
+import com.insight_pulse.tech.campaign.dto.CampaignSettingRequest;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CampaignSettingsService {
+    private final AutomationService automationService;
+    @Transactional
+    public void saveAllSettings(String campaignId, CampaignSettingRequest request) {
+        automationService.updateRules(campaignId, request.getAutomations());
+    }
+}
