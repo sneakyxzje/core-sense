5d46547a530d458c5b4514fd7a7dde3b0d98ed64
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-07 23:22:10 +0700

    feat(campaign): add campaign setting request dto

---
+++ server/src/main/java/com/insight_pulse/tech/campaign/dto/CampaignSettingRequest.java
@@ -0,0 +1,12 @@
+package com.insight_pulse.tech.campaign.dto;
+
+import java.util.List;
+
+import com.insight_pulse.tech.automation.dto.AutomationRequest;
+
+import lombok.Data;
+
+@Data
+public class CampaignSettingRequest {
+    private List<AutomationRequest> automations;
+}
