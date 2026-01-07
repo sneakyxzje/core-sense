e6c96bf2989555f3a21baec00691cd2919bec0b5
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-07 23:19:12 +0700

    feat(automation): implement automation repository

---
+++ server/src/main/java/com/insight_pulse/tech/automation/domain/AutomationRepository.java
@@ -0,0 +1,13 @@
+package com.insight_pulse.tech.automation.domain;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutomationRepository extends JpaRepository<Automation, Long>{
+    Optional<Automation> findByCampaignIdAndEventCode(String campaignId, AutomationEnum eventCode);    
+
+    Optional<Automation> findByCampaignIdAndEventCodeAndFromStageId(String campaignId, AutomationEnum eventCode, String fromStageId);    
+
+    Automation deleteByCampaignId(String campaignId);
+}
