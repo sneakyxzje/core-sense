dcab6c57038fdb24fe1b4deaee6e8ba3bb974a4e
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-08 15:43:09 +0700

    feat(campaign): add campaign setting service

--- server/src/main/java/com/insight_pulse/tech/campaign/service/CampaignSettingsService.java
+++ server/src/main/java/com/insight_pulse/tech/campaign/service/CampaignSettingsService.java
@@ -3,7 +3,7 @@ package com.insight_pulse.tech.campaign.service;
 import org.springframework.stereotype.Service;
 
 import com.insight_pulse.tech.automation.service.AutomationService;
-import com.insight_pulse.tech.campaign.dto.CampaignSettingRequest;
+import com.insight_pulse.tech.campaign.dto.setting.CampaignSettingRequest;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
