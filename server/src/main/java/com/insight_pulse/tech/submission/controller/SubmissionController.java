b311b103af256b5ef697b81e95271deb86f399d9
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-08 15:43:49 +0700

    refactor(submission): update controller base path, simplify multipart handling, and align endpoints with new API structure

--- server/src/main/java/com/insight_pulse/tech/submission/controller/SubmissionController.java
+++ server/src/main/java/com/insight_pulse/tech/submission/controller/SubmissionController.java
@@ -2,8 +2,8 @@ package com.insight_pulse.tech.submission.controller;
 
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.insight_pulse.tech.campaign.dto.PublicCampaignResponse;
 import com.insight_pulse.tech.submission.dto.DeleteStageRequest;
 import com.insight_pulse.tech.submission.dto.SubmissionRequest;
@@ -27,25 +25,17 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/submission")
+@RequestMapping("/api/submissions")
 public class SubmissionController {
  
     private final SubmissionService submissionService;
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    @PostMapping("/{campaignId}")
+    @PostMapping(value = "/{campaignId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> submitForm(@PathVariable String campaignId, 
         @RequestPart("file") MultipartFile file,
-        @RequestPart("data") String dataJson
+        @RequestPart("data") SubmissionRequest request
     ) {
-        try {
-            SubmissionRequest requestDto = objectMapper.readValue(dataJson, SubmissionRequest.class);
-            submissionService.submitForm(campaignId, requestDto, file);
+            submissionService.submitForm(campaignId, request, file);
             return ResponseEntity.ok("Submit thành công");
-        } catch (JsonProcessingException ex) {
-            return ResponseEntity.badRequest().body("JSON type error: " + ex.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("Lỗi xử lý: " + e.getMessage());
-        }
     }
 
     @GetMapping("/campaign-schema/{campaignId}")
@@ -64,9 +54,5 @@ public class SubmissionController {
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("stages/delete")
-    public ResponseEntity<?> deleteStage(@RequestBody DeleteStageRequest request) {
-        submissionService.deleteStageColumn(request);
-        return ResponseEntity.noContent().build(); 
-    }
+
 }
