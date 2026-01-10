89f0cabb765485e93ccfc46874c26bd5642aa783
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-10 17:47:36 +0700

    feat(submission): add endpoints for archive, restore and permanent delete

--- server/src/main/java/com/insight_pulse/tech/submission/controller/SubmissionController.java
+++ server/src/main/java/com/insight_pulse/tech/submission/controller/SubmissionController.java
@@ -2,8 +2,10 @@ package com.insight_pulse.tech.submission.controller;
 
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.insight_pulse.tech.campaign.dto.PublicCampaignResponse;
-import com.insight_pulse.tech.submission.dto.DeleteStageRequest;
 import com.insight_pulse.tech.submission.dto.SubmissionRequest;
+import com.insight_pulse.tech.submission.dto.SubmissionResponse;
 import com.insight_pulse.tech.submission.dto.SubmissionStarRequest;
 import com.insight_pulse.tech.submission.dto.SubmissionSummary;
 import com.insight_pulse.tech.submission.service.SubmissionService;
@@ -54,5 +56,26 @@ public class SubmissionController {
         return ResponseEntity.noContent().build();
     }
 
+    @PostMapping("/{submissionId}/archive")
+    public ResponseEntity<?> archiveSubmission(@PathVariable String submissionId) {
+        submissionService.archiveSubmission(submissionId);
+        return ResponseEntity.noContent().build();
+    } 
 
+    @PostMapping("/{submissionId}/restore")
+    public ResponseEntity<?> restoreSubmission(@PathVariable String submissionId) {
+        submissionService.restoreSubmission(submissionId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{submissionId}/delete")
+    public ResponseEntity<?> deleteSubmission(@PathVariable String submissionId) {
+        submissionService.deleteSubmission(submissionId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping({"/archived"})
+    public ResponseEntity<Page<SubmissionResponse>> getAllArchive(Pageable pageable) {
+        return ResponseEntity.ok(submissionService.getAllArchive(pageable));
+    }
 }
