7c29226cdd2d53dd3e616823dd61f14f89142e76
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-10 17:48:40 +0700

    feat(submission): implement native queries for archive, restore, and hard delete

--- server/src/main/java/com/insight_pulse/tech/submission/domain/SubmissionRepository.java
+++ server/src/main/java/com/insight_pulse/tech/submission/domain/SubmissionRepository.java
@@ -1,7 +1,9 @@
 package com.insight_pulse.tech.submission.domain;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import jakarta.transaction.Transactional;
 
 public interface SubmissionRepository extends JpaRepository<Submission, String> {
     Page<Submission> findAllByCampaignId(String campaignId, Pageable pageable);
@@ -72,4 +76,58 @@ public interface SubmissionRepository extends JpaRepository<Submission, String> 
             SELECT s.cvUrl FROM Submission s WHERE s.id = :submissionId
             """)
     String findCVUrlBySubmissionId(String submissionId);
+
+    Optional<Submission> findByIdAndCampaign_User_Id(String submissionId, int userId);
+
+    @Query(value = """
+                SELECT s.* FROM submissions s
+                JOIN campaigns c ON s.campaigns_id = c.id
+                WHERE deleted_at IS NOT NULL
+                AND c.owner_id = :userId
+            """, 
+            countQuery = 
+            """
+                SELECT count(*) FROM submissions s
+                JOIN campaigns c ON s.campaigns_id = c.id
+                WHERE deleted_at IS NOT NULL
+                AND c.owner_id = :userId
+            """,
+            nativeQuery = true)
+    Page<Submission> findAllArchive(@Param("userId") int userId, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(value = """
+        UPDATE submissions s
+        SET is_deleted = true, 
+            deleted_at = :now
+        FROM campaigns c
+        WHERE s.campaigns_id = c.id
+        AND s.id = :submissionId
+        AND c.owner_id = :userId
+        """, nativeQuery = true)
+    int archive(
+        @Param("submissionId") String submissionId, 
+        @Param("userId") int userId, 
+        @Param("now") LocalDateTime now
+    );
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(value = """
+            UPDATE submissions s 
+            SET is_deleted = false,
+            deleted_at = null
+        FROM campaigns c WHERE s.campaigns_id = c.id
+        AND s.id = :submissionId AND c.owner_id = :userId
+            """, nativeQuery = true)
+    int restore(@Param("submissionId") String submissionId, @Param("userId") int userId);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(value = """
+            DELETE FROM submissions s USING campaigns c
+            WHERE s.campaigns_id = c.id AND s.id = :submissionId AND c.owner_id = :userId AND deleted_at IS NOT NULL
+            """, nativeQuery = true)
+    int hardDelete(@Param("submissionId") String submissionId, @Param("userId") int userId);
 }
