445b7b4d43eadc688a3745990b3b19c8b71464aa
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-13 19:32:09 +0700

    feat(application): enable async

--- server/src/main/java/com/insight_pulse/tech/TechApplication.java
+++ server/src/main/java/com/insight_pulse/tech/TechApplication.java
@@ -3,6 +3,7 @@ package com.insight_pulse.tech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.insight_pulse.tech.security.config.JwtConfigProperties;
@@ -10,6 +11,7 @@ import com.insight_pulse.tech.security.config.JwtConfigProperties;
 @SpringBootApplication
 @EnableConfigurationProperties(JwtConfigProperties.class)
 @EnableScheduling
+@EnableAsync
 public class TechApplication {
 
 	public static void main(String[] args) {
