b6429b0c6ff47498a222497cc29e7160ff034d29
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-15 11:45:02 +0700

    build(config): switch from adapter-auto to adapter-node

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
+import adapter from "@sveltejs/adapter-node";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
