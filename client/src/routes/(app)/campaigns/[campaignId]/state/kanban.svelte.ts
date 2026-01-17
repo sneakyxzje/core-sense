5adf6aed343382b16eef92a59ddfdb4cfbe9cf4a
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-17 15:44:24 +0700

    feat(kanban): separate kanban method from campaign state to kanban state

---
+++ client/src/routes/(app)/campaigns/[campaignId]/state/kanban.svelte.ts
@@ -0,0 +1,133 @@
+import type { CampaignStage } from "@src/lib/types/campaign";
+import type { SubmissionWithStage } from "@src/lib/types/submission";
+import { api } from "@src/lib/utils/api";
+import type { DndEvent } from "svelte-dnd-action";
+import { toast } from "svelte-sonner";
+
+export class KanbanState {
+  columns = $state<CampaignStage[]>([]);
+  private campaignId: string;
+  open = $state(false);
+  private getSubmissions: () => SubmissionWithStage[];
+  private onSubmissionsUpdate: (newItems: SubmissionWithStage[]) => void;
+  isDeletePopupOpen = $state(false);
+  stageToDelete = $state<string | null>(null);
+  targetStageId = $state<string | null>(null);
+  availableStages = $derived(
+    this.columns.filter((s) => s.id !== this.stageToDelete),
+  );
+  constructor(initialData: {
+    columns: CampaignStage[];
+    getSubmissions: () => SubmissionWithStage[];
+    onSubmissionsUpdate: (items: SubmissionWithStage[]) => void;
+    campaignId: string;
+  }) {
+    this.columns = initialData.columns ?? [];
+    this.getSubmissions = initialData.getSubmissions;
+    this.onSubmissionsUpdate = initialData.onSubmissionsUpdate;
+    this.campaignId = initialData.campaignId;
+  }
+
+  addColumn = async () => {
+    try {
+      const response = await api.post<CampaignStage, {}>(
+        `/campaigns/${this.campaignId}/stages`,
+        {
+          stageName: "Cột mới",
+          position: this.columns.length,
+        },
+        fetch,
+      );
+      const newStage = response;
+      this.columns = [...this.columns, newStage];
+      toast.success("Đã tạo cột mới thành công");
+    } catch (error) {
+      toast.error("Không thể tạo cột, vui lòng thử lại");
+    }
+  };
+
+  handleDelete = (colId: string) => {
+    this.stageToDelete = colId;
+    if (this.getSubmissions().some((s) => s.stageId === colId)) {
+      this.isDeletePopupOpen = true;
+    } else {
+      this.deleteColumn(colId, null);
+    }
+  };
+
+  deleteColumn = async (
+    stageToDelete: string | null,
+    targetStage: string | null,
+  ) => {
+    try {
+      await api.delete(
+        `/campaigns/${this.campaignId}/stages/delete`,
+        { stageToDelete: stageToDelete, targetStage: targetStage },
+        fetch,
+      );
+      this.isDeletePopupOpen = false;
+      this.columns = this.columns.filter((c) => c.id !== stageToDelete);
+      if (targetStage) {
+        const updatedSubmissions = this.getSubmissions().map((s) =>
+          s.stageId === stageToDelete ? { ...s, stageId: targetStage } : s,
+        );
+        this.onSubmissionsUpdate(updatedSubmissions);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  private updateSubmissionLocal = (
+    newColumnsItem: SubmissionWithStage[],
+    columnId: string,
+  ) => {
+    const otherSubmissions = this.getSubmissions().filter(
+      (s) => s.stageId !== columnId,
+    );
+
+    this.onSubmissionsUpdate([...otherSubmissions, ...newColumnsItem]);
+  };
+
+  dragSourceColumnId = $state<string | null>(null);
+  onConsider = (
+    columnId: string,
+    e: CustomEvent<DndEvent<SubmissionWithStage>>,
+  ) => {
+    const { items, info } = e.detail;
+    if (!this.dragSourceColumnId) {
+      const movedItem = this.getSubmissions().find((s) => s.id === info.id);
+      this.dragSourceColumnId = movedItem?.stageId || null;
+    }
+    items.forEach((item) => {
+      item.stageId = columnId;
+    });
+  };
+
+  onFinalize = async (
+    columnId: string,
+    e: CustomEvent<DndEvent<SubmissionWithStage>>,
+  ) => {
+    const { items, info } = e.detail;
+    items.forEach((item) => {
+      item.stageId = columnId;
+    });
+    const isChangingColumn =
+      this.dragSourceColumnId !== null && this.dragSourceColumnId !== columnId;
+    this.updateSubmissionLocal(items, columnId);
+    if (isChangingColumn) {
+      try {
+        await api.patch(
+          `/campaigns/${this.campaignId}/submissions/${info.id}/stage`,
+          {
+            stageId: columnId,
+          },
+          fetch,
+        );
+      } catch (error) {
+        toast.error("Lỗi cập nhật server, đang hoàn tác...");
+      }
+    }
+    this.dragSourceColumnId = null;
+  };
+}
