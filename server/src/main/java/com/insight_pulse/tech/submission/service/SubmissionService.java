403e214de5ad92ad2a802b5db89edd62ef44de7c
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-23 09:54:19 +0700

    refactor(submission): switch submissions query to specification-based filtering and add email validation

--- server/src/main/java/com/insight_pulse/tech/submission/service/SubmissionService.java
+++ server/src/main/java/com/insight_pulse/tech/submission/service/SubmissionService.java
@@ -10,9 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -39,16 +39,18 @@ import com.insight_pulse.tech.quotas.service.QuotaService;
 import com.insight_pulse.tech.security.context.CurrentUserProvider;
 import com.insight_pulse.tech.submission.domain.Submission;
 import com.insight_pulse.tech.submission.domain.SubmissionRepository;
+import com.insight_pulse.tech.submission.domain.common.SearchSpecification;
 import com.insight_pulse.tech.submission.dto.DeleteStageRequest;
 import com.insight_pulse.tech.submission.dto.SubmissionChart;
 import com.insight_pulse.tech.submission.dto.SubmissionDetailResponse;
 import com.insight_pulse.tech.submission.dto.SubmissionEvent;
+import com.insight_pulse.tech.submission.dto.SubmissionFilter;
 import com.insight_pulse.tech.submission.dto.SubmissionRequest;
 import com.insight_pulse.tech.submission.dto.SubmissionResponse;
 import com.insight_pulse.tech.submission.dto.SubmissionStarRequest;
 import com.insight_pulse.tech.submission.dto.SubmissionSummary;
 import com.insight_pulse.tech.submission.mapper.SubmissionMapper;
-import com.insight_pulse.tech.utils.PageableUtils;
+import com.insight_pulse.tech.utils.EmailUtils;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
@@ -74,6 +76,9 @@ public class SubmissionService {
 
     @Transactional
     public void submitForm(String campaignId, SubmissionRequest request, MultipartFile file) {
+        if(!EmailUtils.validateEmail(request.answers().getSysEmail())) {
+            throw new RuntimeException("Invalid email");
+        }
         Campaign campaign = campaignRepository.findById(campaignId).orElseThrow(() -> new RuntimeException("Campaign not found"));
         if (campaign.getStatus() != CampaignStatus.ACTIVE) {
             throw new RuntimeException("Campaign này đã đóng hoặc chưa kích hoạt!");
@@ -103,7 +108,6 @@ public class SubmissionService {
             submission.getSubmittedAt()
         );
         messagingTemplate.convertAndSend("/topic/submissions", event);
-        System.out.println("New submission sent to /topic/submissions");
     }
 
     public PublicCampaignResponse getPublicSchema(String campaignId) {
@@ -153,19 +157,17 @@ public class SubmissionService {
         ));
     }
 
-    public CampaignWithSubmissionsResponse findSubmissionByCampaign(String campaignId, String search, Pageable pageable) {
+    public CampaignWithSubmissionsResponse findSubmissionByCampaign(String campaignId, SubmissionFilter filter, Pageable pageable) {
         int userId = currentUserProvider.getCurrentUserId();
         Campaign campaign = campaignRepository.findByIdAndUserId(campaignId, userId).orElseThrow(() -> new RuntimeException("Campaign not found or permission denied"));
-        Page<Submission> submissions;
 
-        if(StringUtils.hasText(search)) {
-            Pageable nativePageable = PageableUtils.convertToNativePageable(pageable);
-            submissions = submissionRepository.searchSubmission(campaignId, search.trim(), nativePageable);
-        }
-        else {
-            submissions = submissionRepository.findAllByCampaignId(campaignId, pageable);
-        }
-        
+        Specification<Submission> spec = Specification
+        .where(SearchSpecification.belongsToCampaign(campaignId))
+        .and(SearchSpecification.byName(filter.search()))
+        .and(SearchSpecification.byScoreRange(filter.minScore(), filter.maxScore()))
+        .and(SearchSpecification.byDateTime(filter.from(), filter.to()));
+
+        Page<Submission> submissions = submissionRepository.findAll(spec, pageable);
         CampaignDetailResponse campaignResponse = campaignMapper.toDetailResponse(campaign);
         Page<SubmissionResponse> submissionResponse = submissions.map(submissionMapper::toResponse);
         return new CampaignWithSubmissionsResponse(
@@ -194,8 +196,8 @@ public class SubmissionService {
         .orElseThrow(() -> new EntityNotFoundException("Không tìm thấy cột này"));
         CampaignStage campaignStage = campaignStageRepository.findById(request.stageId()).orElseThrow(() -> new EntityNotFoundException("Stage not found"));
         if (submission.getCampaign().getUser().getId() != userId) {
-        throw new RuntimeException("Permission denied");
-    }
+            throw new RuntimeException("Permission denied");
+        }
         if (campaignStage.getCampaign().getUser().getId() != userId) {
             throw new RuntimeException("Bạn không có quyền sửa cột này");
         }
@@ -226,47 +228,6 @@ public class SubmissionService {
     public void toggleStarredStatus(String submissionId, SubmissionStarRequest request) {
         submissionRepository.updateStarredStatus(submissionId, request.starred());
     } 
-
-    // public GeminiResponse analyzeAndSave(String submissionId, String userPrompt) {
-    //     Integer userId = currentUserProvider.getCurrentUserId();
-    //     quotaService.validateQuota(userId);
-    //     Submission submission = submissionRepository.findById(submissionId)
-    //             .orElseThrow(() -> new RuntimeException("Not found"));
-        
-    //     String extractCV = "";
-    //     if (submission.getCvUrl() != null && !submission.getCvUrl().isEmpty()) {
-    //         extractCV = pdfExtractor.extractTextFromUrl(submission.getCvUrl());
-    //     }
-
-    //     if (extractCV == null || extractCV.trim().isEmpty()) {
-    //         System.err.println("WARNING: Cannot extract PDF: " + submissionId);
-    //     }
-
-    
-    //     GeminiRequest request = new GeminiRequest(
-    //          submission.getSchemaSnapshot(),
-    //          submission.getAnswers(),
-    //          userPrompt,
-    //          extractCV
-    //     );
-
-    //     GeminiResponse aiResult = geminiService.analyze(request);
-    //     try {
-    //         Map<String, Object> assessmentMap = objectMapper.convertValue(
-    //             aiResult, 
-    //             new TypeReference<Map<String, Object>>() {}
-    //         );
-    //         submission.setAiAssessment(assessmentMap); 
-    //         submission.setScore(aiResult.score());
-            
-    //         submissionRepository.save(submission);
-    //         eventPublisher.publishEvent(new AutomationTriggerEvent(userId ,submission.getCampaign().getId(), submissionId, AutomationEnum.AI_FILTER));
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    //     quotaService.incrementUsage(userId);
-    //     return aiResult;
-    // }
 
     public GeminiResponse analyzeAndSave(String submissionId, String userPrompt) {
         Integer userId = currentUserProvider.getCurrentUserId();
