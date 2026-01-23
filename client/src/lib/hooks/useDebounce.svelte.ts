bf32987377ea5256b7aaa116bc6345511e78377b
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-23 09:39:58 +0700

    fix(debounce): debounce snapshot value to avoid reactive reference issues

--- client/src/lib/hooks/useDebounce.svelte.ts
+++ client/src/lib/hooks/useDebounce.svelte.ts
@@ -1,10 +1,10 @@
 export function useDebounce<T>(valueGetter: () => T, delay: number = 500) {
-  let debounced = $state(valueGetter());
+  let debounced = $state($state.snapshot(valueGetter()));
   $effect(() => {
     const value = valueGetter();
-
+    JSON.stringify(valueGetter());
     const handler = setTimeout(() => {
-      debounced = value;
+      debounced = $state.snapshot(value);
     }, delay);
 
     return () => clearTimeout(handler);
