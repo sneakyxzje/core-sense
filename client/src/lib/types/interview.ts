9881186d45b7de5c1f8ea43e74d88765b1e2a626
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-26 18:02:41 +0700

    feat(interview): implement interview interface

---
+++ client/src/lib/types/interview.ts
@@ -0,0 +1,9 @@
+export interface Interview {
+  id: string;
+  candidateName: string;
+  candidateEmail: string;
+  campaignName: string;
+  scheduleAt: string;
+  location: string;
+  notes: string;
+}
