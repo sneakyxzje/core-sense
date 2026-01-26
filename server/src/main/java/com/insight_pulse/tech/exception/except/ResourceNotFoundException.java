759be716aaae5f14d8a0ed04592b406c82114aaf
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-26 18:05:55 +0700

    chore(exception): change resource not found exception location

--- server/src/main/java/com/insight_pulse/tech/exception/except/ResourceNotFoundException.java
+++ server/src/main/java/com/insight_pulse/tech/exception/except/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.insight_pulse.tech.exception;
+package com.insight_pulse.tech.exception.except;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
