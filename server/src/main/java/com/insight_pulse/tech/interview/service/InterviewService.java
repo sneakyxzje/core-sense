0fee3de43bed5c6ab49a31bdcea039439198c003
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-26 18:09:41 +0700

    feat(interview): add upcoming, get all interviews service and fix create interview service can not be duplicate

--- server/src/main/java/com/insight_pulse/tech/interview/service/InterviewService.java
+++ server/src/main/java/com/insight_pulse/tech/interview/service/InterviewService.java
@@ -1,11 +1,15 @@
 package com.insight_pulse.tech.interview.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.insight_pulse.tech.interview.domain.Interview;
 import com.insight_pulse.tech.interview.domain.InterviewRepository;
+import com.insight_pulse.tech.interview.dto.InterviewDTO;
 import com.insight_pulse.tech.interview.dto.InterviewRequest;
 import com.insight_pulse.tech.interview.dto.InterviewResponse;
 import com.insight_pulse.tech.security.context.CurrentUserProvider;
@@ -31,6 +35,11 @@ public class InterviewService {
         Submission submission = submissionRepository.findById(request.submissionId())
         .orElseThrow(() -> new RuntimeException("Submission not found"));
 
+        Interview checkInterview = interviewRepository.findBySubmission_Id(request.submissionId());
+        if (checkInterview != null) {
+            throw  new RuntimeException("Interview for this submission already exists");
+        }
+
         LocalDateTime parseSchedule = LocalDateTime.parse(request.schedule());
 
         Interview interview = Interview.create(submission, parseSchedule, request.location(), request.notes(), user);
@@ -41,5 +50,36 @@ public class InterviewService {
             savedInterview.getLocation(),
             savedInterview.getNotes()
         );
+    }
+
+    public Page<InterviewDTO> getAllInterview(Pageable pageable) {
+        int userId = currentUserProvider.getCurrentUserId();
+        Page<Interview> interviews = interviewRepository.findAllByUserId(userId, pageable);
+
+        return interviews.map((i) -> new InterviewDTO(
+            i.getId(),
+            i.getSubmission().getFullname(),
+            i.getSubmission().getEmail(),
+            i.getSubmission().getCampaign().getName(),
+            i.getScheduleAt(),
+            i.getLocation(),
+            i.getNotes()
+        ));
+    }
+
+    public List<InterviewDTO> getNext7DaysInterviews() {
+        int userId = currentUserProvider.getCurrentUserId();
+        LocalDateTime start = LocalDateTime.now(); 
+        LocalDateTime end = start.plusDays(6);
+        List<Interview> interviews = interviewRepository.next7DaysInterviews(start, end, userId);
+        return interviews.stream().map((i) -> new InterviewDTO(
+            i.getId(),
+            i.getSubmission().getFullname(),
+            i.getSubmission().getEmail(),
+            i.getSubmission().getCampaign().getName(),
+            i.getScheduleAt(),
+            i.getLocation(),
+            i.getNotes()
+        )).toList();
     }
 }
