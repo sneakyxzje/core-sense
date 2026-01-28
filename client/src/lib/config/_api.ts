d5ec4789127be216d40de0834a4c4b64c8cb2e71
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-28 18:09:22 +0700

    feat(api): get base url

---
+++ client/src/lib/config/_api.ts
@@ -0,0 +1,11 @@
+import { browser } from "$app/environment";
+import { env } from "node:process";
+
+export const getBaseUrl = () => {
+  const rawBaseUrl = browser
+    ? env.PUBLIC_API_URL
+    : env.PUBLIC_INTERNAL_API_URL ||
+      env.PUBLIC_API_URL ||
+      "http://backend:8080/api";
+  return rawBaseUrl ? rawBaseUrl.trim().replace(/(\r\n|\n|\r)/gm, "") : "";
+};
