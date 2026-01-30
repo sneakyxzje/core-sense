c90042b5548318c6f90fbbbfa002cdf079331005
zenitsugocrazy <tuandattobi@gmail.com>
2026-01-30 15:41:52 +0700

    feat(stats): difference percent and button go to campaign

--- client/src/routes/(app)/dashboard/components/StatsCard.svelte
+++ client/src/routes/(app)/dashboard/components/StatsCard.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import * as Card from "$lib/components/ui/card";
+  import Button from "@src/lib/components/ui/button/button.svelte";
 
   let { items } = $props<{
     items: { id: string; title: string; value: string | number }[];
@@ -11,15 +13,32 @@
     <Card.Root
       class="border-base-border-1 bg-base-2/50 shadow-sm hover:border-base-border-hover transition-colors"
     >
-      <Card.Header
-        class="flex flex-row items-center justify-between space-y-0 pb-2"
-      >
-        <Card.Title class="text-sm font-medium text-muted-foreground"
+      <Card.Header class="flex flex-row items-center justify-between ">
+        <Card.Title class="text-sm font-medium text-base-fg-1"
           >{item.title}</Card.Title
         >
       </Card.Header>
       <Card.Content>
-        <div class="text-2xl font-bold text-foreground">{item.value}</div>
+        <div class="text-2xl font-bold text-base-fg-1">{item.value}</div>
+        {#if item.subValue}
+          <div
+            class="text-sm mt-1"
+            class:text-green-500={item.subValue >= 0}
+            class:text-red-500={item.subValue < 0}
+          >
+            {item.subValue >= 0 ? "+" : ""}
+            {item.subValue}% so với tháng trước
+          </div>
+        {/if}
+        {#if item.action}
+          <div class="mt-2">
+            <Button
+              onclick={() => goto("/campaigns")}
+              class="text-sm h-6  font-medium bg-primary-1 text-base-1 hover:bg-primary-hover "
+              >{item.action}
+            </Button>
+          </div>
+        {/if}
       </Card.Content>
     </Card.Root>
   {/each}
