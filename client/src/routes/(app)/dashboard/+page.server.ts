4bca1fda2bd28ac92aa103a29eb47daf48e78cc8
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-05 18:10:09 +0700

    feat(dashboard): handle vnpay payment callback and redirection logic

--- client/src/routes/(app)/dashboard/+page.server.ts
+++ client/src/routes/(app)/dashboard/+page.server.ts
@@ -5,8 +5,16 @@ import {
   type SubmissionChart,
   type TotalSubmissions,
 } from "@src/lib/types/submission";
+import { redirect } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch, url }) => {
+  const responseCode = url.searchParams.get("vnp_ResponseCode");
+
+  if (responseCode === "00") {
+    throw redirect(303, "/dashboard?payment=success");
+  } else if (responseCode && responseCode === "00") {
+    throw redirect(303, "/dashboard?payment=error");
+  }
   const [stats, chart, summary] = await Promise.all([
     api.get<TotalSubmissions>("/campaigns/stats/info", fetch),
     api.get<SubmissionChart[]>("/campaigns/stats/submission-chart", fetch),
