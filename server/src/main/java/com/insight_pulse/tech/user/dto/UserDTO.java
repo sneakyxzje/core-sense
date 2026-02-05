6533177e4c954fdebaff2864e67fab7413a3e89f
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-05 18:53:52 +0700

    feat(user): add usedcount, plan name, ai limit and subscriptionid properties

--- server/src/main/java/com/insight_pulse/tech/user/dto/UserDTO.java
+++ server/src/main/java/com/insight_pulse/tech/user/dto/UserDTO.java
@@ -1,9 +1,14 @@
 package com.insight_pulse.tech.user.dto;
 
+
 public record UserDTO(
     int id,
     String fullname,
-    String email
+    String email,
+    int usedCount,
+    String planName,
+    int aiLimit,
+    String subscriptionId
 ) {
     
 }
