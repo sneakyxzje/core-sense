d97be3d0b52414b96d1d5a9c297602489487bf08
zenitsugocrazy <tuandattobi@gmail.com>
2026-02-05 18:53:32 +0700

    feat(user): enrich userdto with subscription and quota details

--- server/src/main/java/com/insight_pulse/tech/user/service/UserService.java
+++ server/src/main/java/com/insight_pulse/tech/user/service/UserService.java
@@ -2,8 +2,12 @@ package com.insight_pulse.tech.user.service;
 
 import org.springframework.stereotype.Service;
 
+import com.insight_pulse.tech.quotas.dto.QuotaResponse;
+import com.insight_pulse.tech.quotas.service.QuotaService;
+import com.insight_pulse.tech.security.principal.UserDetailsImpl;
 import com.insight_pulse.tech.user.domain.User;
 import com.insight_pulse.tech.user.domain.UserRepository;
+import com.insight_pulse.tech.user.dto.UserDTO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
     
     private final UserRepository userRepository;
-
+    private final QuotaService quotaService;
     public User processOAuthPostLogin(String email, String name, String googleId) {
         return userRepository.findByGoogleId(googleId).orElseGet(() -> {
             User newUser = new User();
@@ -21,5 +25,18 @@ public class UserService {
             newUser.setGoogleId(googleId);
             return userRepository.save(newUser);
         });
+    }
+
+    public UserDTO getUser(UserDetailsImpl currentUser) {
+        QuotaResponse quota = quotaService.getQuota(currentUser.getId());
+        return new UserDTO(
+            currentUser.getId(),
+            currentUser.getFullname(),
+            currentUser.getEmail(),
+            quota.usedCount(),
+            quota.subscription().getName(),
+            quota.subscription().getAiLimit(),
+            quota.subscription().getId()
+        );
     }
 }
